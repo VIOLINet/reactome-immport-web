1425d201d80b83006d00953054b98c0f99e03699
Tim Brunson <tjbrunson@gmail.com>
2021-01-21 11:29:48 -0800

    restructured files and added panel for description

---
+++ src/components/ImmportResults/Description/ResultsDescriptionPanel.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card outlined>
+      <v-card-title>
+          Description
+          <v-spacer></v-spacer>
+          <v-btn icon @click="show = !show">
+            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+          </v-btn>
+      </v-card-title>
+      <v-expand-transition>
+          <v-card-text v-show="show">
+              <ResultsDescription :formData="formData"/>
+          </v-card-text>
+      </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+import ResultsDescription from './ResultsDescription'
+export default {
+    name: "ResultsDescriptionPanel",
+    components: {
+        ResultsDescription
+    },
+    props: {
+        formData: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    data: () => ({
+        show: false,
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
