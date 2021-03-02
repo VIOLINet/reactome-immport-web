298f16f16953f474d8e6ecdefe3f013a04b16538
Tim Brunson <tjbrunson@gmail.com>
2021-03-02 10:45:20 -0800

    added title so hover shows full text when elipsed

--- src/components/ImmportResults/Description/ResultsDescription.vue
+++ src/components/ImmportResults/Description/ResultsDescription.vue
@@ -1,53 +1,53 @@
 <template>
-  <v-sheet color="grey lighten-2 ma-3">
+  <v-sheet color="grey lighten-2">
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Vaccines:</b> {{formData.selectedVaccines.join(", ")}}</v-list-item-title>
+        <v-list-item-title :title="formData.selectedVaccines.join(', ')"><b>Vaccines:</b> {{formData.selectedVaccines.join(", ")}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Studies:</b> {{formData.selectedStudies.join(", ")}}</v-list-item-title>
+        <v-list-item-title :title="formData.selectedStudies.join(', ')"><b>Studies:</b> {{formData.selectedStudies.join(", ")}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Platform Descriptions:</b> {{formData.selectedPlatforms.join(", ")}}</v-list-item-title>
+        <v-list-item-title :title="formData.selectedPlatforms.join(', ')"><b>Platform Descriptions:</b> {{formData.selectedPlatforms.join(", ")}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Day 0 Definitions:</b> {{formData.selectedDayDefinitions.join(", ")}}</v-list-item-title>
+        <v-list-item-title :title="formData.selectedDayDefinitions.join(', ')"><b>Day 0 Definitions:</b> {{formData.selectedDayDefinitions.join(", ")}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Genders:</b> {{formData.selectedGenders.join(", ")}}</v-list-item-title>
+        <v-list-item-title :title="formData.selectedGenders.join(', ')"><b>Genders:</b> {{formData.selectedGenders.join(", ")}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Ages:</b> {{formData.selectedAges.join(", ")}}</v-list-item-title>
+        <v-list-item-title :title="formData.selectedAges.join(', ')"><b>Ages:</b> {{formData.selectedAges.join(", ")}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Race:</b> {{formData.selectedRaces.join(", ")}}</v-list-item-title>
+        <v-list-item-title :title="formData.selectedRaces.join(', ')"><b>Race:</b> {{formData.selectedRaces.join(", ")}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Study Cohort:</b> {{formData.studyCohort.length > 0 ?formData.studyCohort.join(", ") : "None"}}</v-list-item-title>
+        <v-list-item-title :title="formData.studyCohort.length > 0 ?formData.studyCohort.join(', ') : 'None'"><b>Study Cohort:</b> {{formData.studyCohort.length > 0 ?formData.studyCohort.join(", ") : "None"}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Platform Correction:</b> {{formData.platformCorrection === true ? "Yes" : "No"}}</v-list-item-title>
+        <v-list-item-title :title="formData.platformCorrection === true ? 'Yes' : 'No'"><b>Platform Correction:</b> {{formData.platformCorrection === true ? "Yes" : "No"}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     <v-list-item dense class="text-left">
       <v-list-item-content>
-        <v-list-item-title><b>Use time directly in the model?</b> {{formData.modelTime === true ? "Yes" : "No"}}</v-list-item-title>
+        <v-list-item-title :title="formData.modelTime === true ? 'Yes' : 'No'"><b>Use time directly in the model?</b> {{formData.modelTime === true ? "Yes" : "No"}}</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
   </v-sheet>
