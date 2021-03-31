6680c68adb819541fa6edaa0c8768144b8f32637
Tim Brunson <tjbrunson@gmail.com>
2021-03-31 08:55:20 -0700

    added legend for comparison color explanation

---
+++ src/components/ImmportResults/Cytoscape/ComparisonLegend.vue
@@ -0,0 +1,54 @@
+<template>
+  <div style="">
+      <div class="flex">
+          <p>Result a node</p>
+          <div class="nodeCircle" style="background-color:var(--node-a-color)"></div>
+      </div>
+      <div class="flex">
+          <p>Result b node</p>
+          <div class="nodeCircle" style="background-color:var(--node-b-color)"></div>
+      </div>
+      <div class="flex">
+          <p>Result a & b node</p>
+          <div class="nodeCircle" style="background-color:var(--node-a-b-color)"></div>
+      </div>
+      <div class="flex">
+          <p>Result a edge</p>
+          <div class="edge" style="background-color:var(--edge-a-color)"></div>
+      </div>
+      <div class="flex">
+          <p>Result b edge</p>
+          <div class="edge" style="background-color:var(--edge-b-color)"></div>
+      </div>
+      <div class="flex">
+          <p>Result a & b edge</p>
+          <div class="edge" style="background-color:var(--edge-a-b-color)"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+p {
+    margin:0 20px 0 0 !important;
+}
+.flex {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.nodeCircle {
+    border-radius: 16px;
+    height:16px;
+    width:16px;
+}
+.edge {
+    height: 3px;
+    width: 16px;
+}
+</style>
