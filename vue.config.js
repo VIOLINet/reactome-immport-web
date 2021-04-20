6ea8181fc2d6713bd43e29fd1615a1f60c049986
Tim Brunson <tjbrunson@gmail.com>
2021-04-20 10:40:04 -0700

    set public path so output index.html file is relative

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 var FriendlyErrorsPlugin = require("friendly-errors-plugin");
 
 module.exports = {
+  publicPath:'',
   configureWebpack: {
     devtool: "source-map",
     plugins: [new FriendlyErrorsPlugin()],
