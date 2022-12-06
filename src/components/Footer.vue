f9a0f78ec8e05623c1e926d561269a13c85c6978
guanmingwu <guanmingwu@gmail.com>
2022-12-06 15:58:08 -0800

    added disclaimer text at the bottom

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,10 @@
       Diseases of the National Institutes of Health under Award Number 
       <a target="_blank" href="https://reporter.nih.gov/search/J1AGcIXKfkmYoykI5X9WxA/project-details/9810625">UH2AI132931</a>. 
       The content is solely the responsibility of the app developers and does not necessarily 
-      represent the official views of the National Institutes of Health.</p>
+      represent the official views of the National Institutes of Health. The VIGET program is freely available for 
+      the public to use. The VIGET development team has made every effort possible to ensure that VIGET performs 
+      as expected. We accept no responsibility for the accuracy, sufficiency, reliability, or for any loss resulting 
+      from the use of the VIGET program.</p>
   </div>
 </template>
 
