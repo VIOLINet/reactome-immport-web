a21200d88b4c218aa9310d252f7325e37028d471
guanmingwu <guanmingwu@gmail.com>
2023-02-27 09:26:01 -0800

    update URL for VIOLINET's vaccine ontology

--- src/components/Docs.vue
+++ src/components/Docs.vue
@@ -8,7 +8,7 @@
       The list of selected differentially expressed genes between two groups can then be submitted to <a target="_blank" href="https://reactome.org">
       Reactome</a>, an open access biological pathway knowledgebase, for pathway enrichment and network analyses. The results from these 
       analyses are displayed here for visualization. The vaccines are organized based on the vaccine ontology hosted at  
-      <a target="_blank" href="http://www.violinet.org/vaccineontology/index.php">VIOLINET</a>, a web-based vaccine database and analysis platform.
+      <a target="_blank" href="https://violinet.org/vaccineontology/index.php">VIOLINET</a>, a web-based vaccine database and analysis platform.
       The original data was collected from <a target="_blank" href="https://www.immport.org/home">ImmPort</a>, a NIH supported database hosting 
       immunological studies, and then pre-processed locally.
     </p>
